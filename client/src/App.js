c5311c69552b2735c224c89535d9c042f5249078
EricLiam94 <ericliamhjx@gmail.com>
2019-11-15 22:24:17 +1100

    ss

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Users from './components/users.js'
+import Nav from './components/nav.js'
 
 function App() {
   return (
     <div className="App">
+    <Nav/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
        <Users/>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="/"
           target="_blank"
           rel="noopener noreferrer"
         >
--- server.js
+++ server.js
@@ -15,18 +15,17 @@ app.get("/api/customer", (req, res) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-
+  app.use(express.static("client/build"));
   app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
-
-  app.get("/test",(req,res)=>{
-      res.send("This is a test")
-  })
 }
 
-const port = process.env.PORT || 5000
+app.get("/test", (req, res) => {
+  res.send("This is a test");
+});
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log("port 5000 is connected");
