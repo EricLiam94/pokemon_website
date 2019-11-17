b3f7d2d45c3a09988734f32dcd59506ef9279cb7
EricLiam94 <ericliamhjx@gmail.com>
2019-11-17 17:56:20 +1100

    sss

--- client/src/components/arrowdown.js
+++ client/src/components/arrowdown.js
@@ -9,15 +9,15 @@ function ArrowDown(props)
 {
     const colors = ['#bdc9c5','#6fc7aa','#0cc789']
     const [idx ,setIdx] = useState(props.idx)
-    useState(()=>{
-            setInterval(() => {
-                setIdx(idx=>idx+1)
+    useEffect(()=>{
+         const interval =  setInterval(() => {
+                setIdx(idx=> (idx+1)%3 )
             }, 300);
-
+        return () => clearInterval(interval);
     },[])
 
     return(
-            <div style={{color:colors[idx%3]}}> ▼ </div>
+            <div style={{color:colors[idx]}}> ▼ </div>
     )
 }
 
